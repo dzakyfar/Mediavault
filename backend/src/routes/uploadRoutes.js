a9aec1795a002a8e310569df502ff0334861131e
dzakyfar <dzakyfr509@gmail.com>
2026-06-10 09:02:48 +0700

    update

--- backend/src/routes/uploadRoutes.js
+++ backend/src/routes/uploadRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createUploadUrl, getDownloadUrl, uploadDirect } = require('../controllers/uploadController');
+const { createUploadUrl, getDownloadUrl, uploadDirect, proxyDownload } = require('../controllers/uploadController');
 const { protect } = require('../middleware/authMiddleware');
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post('/presign', protect, createUploadUrl);
 router.post('/direct', protect, uploadDirect);
 router.get('/download-url', protect, getDownloadUrl);
+router.get('/proxy-download', protect, proxyDownload);
 
 module.exports = router;
